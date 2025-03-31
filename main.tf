3cdffc7cc0444eb11f6015ca329af37ca1a983f7
paraggit-tech <parag.panda1987@gmail.com>
2025-03-31 14:52:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
+terraform {
+  cloud {
+    organization = "hell321"
+
+    workspaces {
+      name = "terraform_project"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "one" {
-  ami           = "ami-02f624c08a83ca16f"
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+
   tags = {
-    Name = "hcp-server"
+    Name = "Terraform-EC2"
   }
 }
