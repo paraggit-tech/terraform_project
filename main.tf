f83f9d55c7b4bc6cc17d7b9f96404136c6940519
paraggit-tech <parag.panda1987@gmail.com>
2025-04-01 14:07:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,8 @@
-terraform {
-  cloud {
-    organization = "hell321"
-
-    workspaces {
-      name = "terraform_project"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-02f624c08a83ca16f"
   instance_type = "t2.micro"
+
+  iam_instance_profile = "TerraformEC2Role"
 
   tags = {
     Name = "Terraform-EC2"
