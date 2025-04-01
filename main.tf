598d8deb277da3aa4503d203bbb11a9dedf7c113
paraggit-tech <parag.panda1987@gmail.com>
2025-04-01 15:54:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_instance" "example" {
    ami = "ami-071226ecf16aa7d96"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
-
-  tags = {
+tags = {
     Name = "Terraform-EC2"
   }
 }
