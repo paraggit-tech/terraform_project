04081b5126d031483ffa93478d3be46d516eb670
paraggit-tech <parag.panda1987@gmail.com>
2025-04-01 12:06:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-071226ecf16aa7d96"
   instance_type = "t2.micro"
 
   tags = {
