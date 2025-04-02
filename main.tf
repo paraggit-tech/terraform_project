26cd43354fd9a2686b767f466cd0db7e2186f202
paraggit-tech <parag.panda1987@gmail.com>
2025-04-02 15:40:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "example" {
    ami = "ami-02f624c08a83ca16f"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
 tags = {
-    Name = "Terraform-EC2"
+    Name = "HCP"
   }
 }
