79639cb643516c5b2494033c54ded36b5ac626ce
paraggit-tech <parag.panda1987@gmail.com>
2025-04-03 14:17:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"
 }
-resource "aws_instance" "example" {
-   ami = "ami-02f624c08a83ca16f"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"
-tags = {
-    Name = "HCP"
-  }
+resource "aws_instance" "one" {
+count = 5
+ami = "ami-03eb6185d756497f8"
+instance_type = "t2.micro"
 }
